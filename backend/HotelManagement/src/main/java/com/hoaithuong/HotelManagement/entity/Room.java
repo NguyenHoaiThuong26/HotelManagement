831f216e105956d43ac82a511f462dd4b270ec41
NguyenHoaiThuong26 <21130559@st.hcmuaf.edu.vn>
2025-05-21 15:37:01 +0700

    create entity and model

---
+++ backend/HotelManagement/src/main/java/com/hoaithuong/HotelManagement/entity/Room.java
@@ -0,0 +1,36 @@
+package com.hoaithuong.HotelManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roomType;
+    private String roomPrice;
+    private String roomPhotoUrl;
+    private String roomDescription;
+
+    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Booking> bookings = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", roomType='" + roomType + '\'' +
+                ", roomPrice='" + roomPrice + '\'' +
+                ", roomPhotoUrl='" + roomPhotoUrl + '\'' +
+                ", roomDescription='" + roomDescription + '\'' +
+                '}';
+    }
+}
