0b547d0ef0393c2ada497f9df9aef0eb9c364b73
NguyenHoaiThuong26 <21130559@st.hcmuaf.edu.vn>
2025-05-30 22:22:23 +0700

    fix key

--- backend/HotelManagement/src/main/java/com/hoaithuong/HotelManagement/config/FirebaseConfig.java
+++ backend/HotelManagement/src/main/java/com/hoaithuong/HotelManagement/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ package com.hoaithuong.HotelManagement.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -13,11 +14,14 @@ import java.io.IOException;
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.config.path}")
+    private String firebaseConfigPath;
+
     @PostConstruct
     public void initialize() {
         try {
             FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/firebase/serviceAccountKey.json");
+                    new FileInputStream(firebaseConfigPath);
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
