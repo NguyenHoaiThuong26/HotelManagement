330d2f97fa4c9d46c1a0a4452d997a2cac599583
NguyenHoaiThuong26 <21130559@st.hcmuaf.edu.vn>
2025-05-30 22:12:40 +0700

    add room controller, config firebase

--- backend/HotelManagement/src/main/java/com/hoaithuong/HotelManagement/repo/BookingRepository.java
+++ backend/HotelManagement/src/main/java/com/hoaithuong/HotelManagement/repo/BookingRepository.java
@@ -3,6 +3,9 @@ package com.hoaithuong.HotelManagement.repo;
 import com.hoaithuong.HotelManagement.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
 }
